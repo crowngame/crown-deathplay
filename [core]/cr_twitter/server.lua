c234e17a420a966d6e04cf92af486ff574e07ef1
Farid Salimov <129788455+faridsalimov@users.noreply.github.com>
2024-05-26 18:38:57 +0300

    Update twitter.

--- [core]/cr_twitter/server.lua
+++ [core]/cr_twitter/server.lua
@@ -3,16 +3,20 @@ addEventHandler("twitter.sendTweet", root, function(text)
 	if client ~= source then return end
 	if exports.cr_global:hasMoney(client, 1000) then
 		if string.len(text) > 0 then
-			exports.cr_global:takeMoney(client, 1000)
-			triggerClientEvent(client, "playSuccessfulSound", client)
-			
-			for _, player in pairs(getElementsByType("player")) do
-				if (getElementData(player, "loggedin") == 1) then
-					outputChatBox("[Twitter] " .. text .. " @" .. getPlayerName(client):gsub("_", " "), player, 0, 178, 238)
+			if not getElementData(client, "adminjailed") then
+				exports.cr_global:takeMoney(client, 1000)
+				triggerClientEvent(client, "playSuccessfulSound", client)
+				
+				for _, player in pairs(getElementsByType("player")) do
+					if (getElementData(player, "loggedin") == 1) then
+						outputChatBox("[Twitter] " .. text .. " @" .. getPlayerName(client):gsub("_", " "), player, 0, 178, 238)
+					end
 				end
+				
+				exports.cr_discord:sendMessage("twitter-log", "[Twitter] " .. text .. " @" .. getPlayerName(client):gsub("_", " "))
+			else
+				exports.cr_infobox:addBox(client, "error", "Jailde iken tweet atamazsınız.")
 			end
-			
-			exports.cr_discord:sendMessage("twitter-log", "[Twitter] " .. text .. " @" .. getPlayerName(client):gsub("_", " "))
 		else
 			exports.cr_infobox:addBox(client, "error", "İçerik boş bırakılamaz.")
 		end
