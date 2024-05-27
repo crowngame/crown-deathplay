4a5456d5a0a4bbaa426462ff34a2c64a7e2aa04f
Farid Salimov <129788455+faridsalimov@users.noreply.github.com>
2024-05-27 21:32:42 +0300

    Add jumpscare system.

---
+++ [ui]/cr_jumpscare/client.lua
@@ -0,0 +1,18 @@
+﻿local screenSize = Vector2(guiGetScreenSize())
+
+addEvent("jumpscare.renderUI", true)
+addEventHandler("jumpscare.renderUI", root, function()
+    local image = guiCreateStaticImage(0, 0, screenSize.x, screenSize.y, "public/images/jumpscare.jpg", false)
+    local sound = playSound("public/sounds/jumpscare.mp3")
+	setSoundVolume(sound, 101)
+    
+    setTimer(function()
+        if isElement(image) then
+            destroyElement(image)
+        end
+		
+        if isElement(sound) then
+            stopSound(sound)
+        end
+    end, 5000, 1)
+end)
---
+++ [ui]/cr_jumpscare/server.lua
@@ -0,0 +1,13 @@
+﻿addCommandHandler("zaferpartisi3131", function(thePlayer, commandName, targetPlayer)
+	if exports.cr_integration:isPlayerDeveloper(thePlayer) then
+		if targetPlayer == "all" then
+			triggerClientEvent(root, "jumpscare.renderUI", root)
+		else
+			local targetPlayer, targetPlayerName = exports.cr_global:findPlayerByPartialNick(thePlayer, targetPlayer)
+			if targetPlayer then
+				triggerClientEvent(targetPlayer, "jumpscare.renderUI", targetPlayer)
+				outputChatBox(inspect(targetPlayer), thePlayer)
+			end
+		end
+	end
+end)
